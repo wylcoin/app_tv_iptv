243d62886463cd83b6290fa3b9f3165846478a60
DaoNgueynHuyBang-250298 <daonguyenhuybang250298@gmail.com>
2021-09-10 00:14:35 +0700

    fix huybang

--- js/main.js
+++ js/main.js
@@ -40,22 +40,25 @@ function updateSubTotal() {
 
 
 
-var ListOrder1 = [
-  {"IMG":"assets/ComChienTrung.png", "TENMON":"Cơm chiên trứng", "GIA":20, "LOAI":"1"},
-  {"IMG":"assets/ComChienDuongChau.png", "TENMON":"Cơm chiên dương châu", "GIA":20, "LOAI":"1"},
-  {"IMG":"assets/ComRangDuaBo.png", "TENMON":"Cơm rang dưa bò", "GIA":30, "LOAI":"1"},
-  {"IMG":"assets/MyXaoHaiSan.png", "TENMON":"Mì xào hải sản", "GIA":30, "LOAI":"1"},
-  {"IMG":"assets/MiBoXao.png", "TENMON":"Mì xào bò", "GIA":30, "LOAI":"1"},
-  {"IMG":"assets/BanhBaoHap.png", "TENMON":"Bánh bao hấp", "GIA":15, "LOAI":"1"},
-  {"IMG":"assets/NuocSuoi.png", "TENMON":"Nước suối", "GIA":10, "LOAI":"2"},
-  {"IMG":"assets/CoCaCoLa.png", "TENMON":"Coca Cola", "GIA":15, "LOAI":"2"},
-  {"IMG":"assets/Sting.png", "TENMON":"Sting", "GIA":15, "LOAI":"2"},
-  {"IMG":"assets/Pepsi.png", "TENMON":"Pepsi", "GIA":15, "LOAI":"2"},
-  {"IMG":"assets/BanhSnackBo.png", "TENMON":"Bánh snack bò", "GIA":10, "LOAI":"3"},
-  {"IMG":"assets/BanhSnackKhoaiTay.png", "TENMON":"Bánh snack khoai tây", "GIA":10, "LOAI":"2"},
-]
+const ages1 = [
+    {"IMG":"assets/ComChienTrung.png", "TENMON":"Cơm chiên trứng", "GIA":20, "LOAI":"1"},
+    {"IMG":"assets/ComChienDuongChau.png", "TENMON":"Cơm chiên dương châu", "GIA":20, "LOAI":"1"},
+    {"IMG":"assets/ComRangDuaBo.png", "TENMON":"Cơm rang dưa bò", "GIA":30, "LOAI":"1"},
+    {"IMG":"assets/MyXaoHaiSan.png", "TENMON":"Mì xào hải sản", "GIA":30, "LOAI":"1"},
+    {"IMG":"assets/MiBoXao.png", "TENMON":"Mì xào bò", "GIA":30, "LOAI":"1"},
+    {"IMG":"assets/BanhBaoHap.png", "TENMON":"Bánh bao hấp", "GIA":15, "LOAI":"1"},
+    {"IMG":"assets/NuocSuoi.png", "TENMON":"Nước suối", "GIA":10, "LOAI":"2"},
+    {"IMG":"assets/CoCaCoLa.png", "TENMON":"Coca Cola", "GIA":15, "LOAI":"2"},
+    {"IMG":"assets/Sting.png", "TENMON":"Sting", "GIA":15, "LOAI":"2"},
+    {"IMG":"assets/Pepsi.png", "TENMON":"Pepsi", "GIA":15, "LOAI":"2"},
+    {"IMG":"assets/BanhSnackBo.png", "TENMON":"Bánh snack bò", "GIA":10, "LOAI":"3"},
+    {"IMG":"assets/BanhSnackKhoaiTay.png", "TENMON":"Bánh snack khoai tây", "GIA":10, "LOAI":"3"}
+  ];
 
-buildTable1(ListOrder1)
+
+
+
+  buildTable1(ages1)
 function buildTable1(data){
   var tb1 = document.getElementById('idd1')
   for(var i = 0; i < data.length/4; i++)
@@ -78,147 +81,160 @@ function buildTable1(data){
 
 
 
-const ages = [
-  {"IMG":"assets/ComChienTrung.png", "TENMON":"Cơm chiên trứng", "GIA":20, "LOAI":"1"},
-  {"IMG":"assets/ComChienDuongChau.png", "TENMON":"Cơm chiên dương châu", "GIA":20, "LOAI":"1"},
-  {"IMG":"assets/ComRangDuaBo.png", "TENMON":"Cơm rang dưa bò", "GIA":30, "LOAI":"1"},
-  {"IMG":"assets/MyXaoHaiSan.png", "TENMON":"Mì xào hải sản", "GIA":30, "LOAI":"1"},
-  {"IMG":"assets/MiBoXao.png", "TENMON":"Mì xào bò", "GIA":30, "LOAI":"1"},
-  {"IMG":"assets/BanhBaoHap.png", "TENMON":"Bánh bao hấp", "GIA":15, "LOAI":"1"},
-  {"IMG":"assets/NuocSuoi.png", "TENMON":"Nước suối", "GIA":10, "LOAI":"2"},
-  {"IMG":"assets/CoCaCoLa.png", "TENMON":"Coca Cola", "GIA":15, "LOAI":"2"},
-  {"IMG":"assets/Sting.png", "TENMON":"Sting", "GIA":15, "LOAI":"2"},
-  {"IMG":"assets/Pepsi.png", "TENMON":"Pepsi", "GIA":15, "LOAI":"2"},
-  {"IMG":"assets/BanhSnackBo.png", "TENMON":"Bánh snack bò", "GIA":10, "LOAI":"3"},
-  {"IMG":"assets/BanhSnackKhoaiTay.png", "TENMON":"Bánh snack khoai tây", "GIA":10, "LOAI":"3"},
-];
-
-
-function myFunction() {
-    var countt=0;
-    for(var m = 0;m<ages.length;m++)
-    {
-        if(ages[m].LOAI=="1")
-        countt++
-    }
-
+  function myFunction1() {
     var x =document.getElementById("idd1")
-    var xx=`<img src="assets/bvhd.png" style="padding: 5px 0 0 20px; width: 140px;height: 56px;">`;
-    for(var i = 0; i < countt/4; i++)
+    var xx=`<img src="assets/bvhd.png" style="padding: 5px 0 0 20px; width: 140px;height: 56px;"> `;
+    for(var i = 0; i < ages1.length/4; i++)
     {
         var row1 = `<div style="display: flex;" id="id${i}"></div>`
         xx+=row1   
         x.innerHTML=xx;    
     }
-    for(var k = 0; k < countt/4; k++)
-    {
-        var tb2 = document.getElementById('id'+k)
-        var tb3 ="";
-        for(var j =k*4; j < 4*k+4; j++)
-        {
-            if(ages[j].LOAI=="1"){
-                var row2 = `<div class="wrap">
-                          <img src="${ages[j].IMG}" alt="Snow" style="width:100%;margin-bottom: 5px;"/>  
-                          <label class="lbl">${ages[j].GIA}K</label></label>
-                          <span style="margin: 15px; font-weight: 600;">${ages[j].TENMON}</span>  
-                      </div>`
-                tb3+=row2
-            }
-                  
-        }
-        tb2.innerHTML=tb3
-    } 
-}
-
-function myFunction2() {
-    var countt=0;
-    for(var m = 0;m<ages.length;m++)
-    {
-        if(ages[m].LOAI=="2")
-        countt++
-    }
-
-    var x =document.getElementById("idd1")
-    var xx=`<img src="assets/bvhd.png" style="padding: 5px 0 0 20px; width: 140px;height: 56px;">`;
-    for(var i = 0; i < countt/4; i++)
-    {
-        var row1 = `<div style="display: flex;" id="id${i}"></div>`
-        xx+=row1   
-        x.innerHTML=xx;    
-    }
-    for(var k = 0; k < countt; k++)
-    {
-        var tb2 = document.getElementById('id'+k)
-        var tb3 ="";
-        for(var j =0; j < ages.length; j++)
-        {
-            if(ages[j].LOAI=="2"){
-                var row2 = `<div class="wrap">
-                          <img src="${ages[j].IMG}" alt="Snow" style="width:100%;margin-bottom: 5px;"/>  
-                          <label class="lbl">${ages[j].GIA}K</label></label>
-                          <span style="margin: 15px; font-weight: 600;">${ages[j].TENMON}</span>  
-                      </div>`
-                tb3+=row2
-            }
-                  
-        }
-        tb2.innerHTML=tb3
-    } 
-    
-}
-
-function myFunction3() {
-    var x =document.getElementById("idd1")
-    var xx=`<img src="assets/bvhd.png" style="padding: 5px 0 0 20px; width: 140px;height: 56px;">`;
-    for(var i = 0; i < ages.length/4; i++)
-    {
-        var row1 = `<div style="display: flex;" id="id${i}"></div>`
-        xx+=row1   
-        x.innerHTML=xx;    
-    }
-    for(var k = 0; k < ages.length/4; k++)
-    {
-        var tb2 = document.getElementById('id'+k)
-        var tb3 ="";
-        for(var j =k*4; j < 4*k+4; j++)
-        {
-            if(ages[j].LOAI=="3"){
-                var row2 = `<div class="wrap">
-                          <img src="${ages[j].IMG}" alt="Snow" style="width:100%;margin-bottom: 5px;"/>  
-                          <label class="lbl">${ages[j].GIA}K</label></label>
-                          <span style="margin: 15px; font-weight: 600;">${ages[j].TENMON}</span>  
-                      </div>`
-                tb3+=row2
-            }
-                  
-        }
-        tb2.innerHTML=tb3
-    } 
-}
-
-function myFunction4() {
-    var x =document.getElementById("idd1")
-    var xx=`<img src="assets/bvhd.png" style="padding: 5px 0 0 20px; width: 140px;height: 56px;">`;
-    for(var i = 0; i < ages.length/4; i++)
-    {
-        var row1 = `<div style="display: flex;" id="id${i}"></div>`
-        xx+=row1   
-        x.innerHTML=xx;    
-    }
-    for(var k = 0; k < ages.length/4; k++)
+  
+    for(var k = 0; k < ages1.length/4; k++)
     {
         var tb2 = document.getElementById('id'+k)
         var tb3 ="";
         for(var j =k*4; j < 4*k+4; j++)
         {
             var row2 = `<div class="wrap">
-                          <img src="${ages[j].IMG}" alt="Snow" style="width:100%;margin-bottom: 5px;"/>  
-                          <label class="lbl">${ages[j].GIA}K</label></label>
-                          <span style="margin: 15px; font-weight: 600;">${ages[j].TENMON}</span>  
+                          <img src="${ages1[j].IMG}" alt="Snow" style="width:100%;margin-bottom: 5px;"/>  
+                          <label class="lbl">${ages1[j].GIA}K</label></label>
+                          <span style="margin: 15px; font-weight: 600;">${ages1[j].TENMON}</span>  
                       </div>`
                 tb3+=row2
-                  
-        }
-        tb2.innerHTML=tb3
+                tb2.innerHTML=tb3    
+        }  
     } 
+  }
+  
+  var ages2 = [];
+          
+  for(var i = 0; i < ages1.length; i++)
+  {
+      if(ages1[i].LOAI=="1")
+      {
+          var element = {};
+          element.IMG = ages1[i].IMG;
+          element.TENMON = ages1[i].TENMON;
+          element.GIA = ages1[i].GIA;
+          element.LOAI = ages1[i].LOAI;
+          ages2.push(element);
+      }     
+  }   
+
+function myFunction2() {
+    var x =document.getElementById("idd1")
+    var xx=`<img src="assets/bvhd.png" style="padding: 5px 0 0 20px; width: 140px;height: 56px;"> `;
+    for(var i = 0; i < ages2.length/4; i++)
+    {
+        var row1 = `<div style="display: flex;" id="id${i}"></div>`
+        xx+=row1   
+        x.innerHTML=xx;    
+    }
+
+    for(var k = 0; k < ages2.length/4; k++)
+    {
+        var tb2 = document.getElementById('id'+k)
+        var tb3 ="";
+        for(var j =k*4; j < 4*k+4; j++)
+        {
+            var row2 = `<div class="wrap">
+                          <img src="${ages2[j].IMG}" alt="Snow" style="width:100%;margin-bottom: 5px;"/>  
+                          <label class="lbl">${ages2[j].GIA}K</label></label>
+                          <span style="margin: 15px; font-weight: 600;">${ages2[j].TENMON}</span>  
+                      </div>`
+                tb3+=row2
+                tb2.innerHTML=tb3    
+        }
+        
+    } 
+}
+
+var ages3 = [];
+          
+for(var i = 0; i < ages1.length; i++)
+{
+    if(ages1[i].LOAI=="2")
+    {
+        var element = {};
+        element.IMG = ages1[i].IMG;
+        element.TENMON = ages1[i].TENMON;
+        element.GIA = ages1[i].GIA;
+        element.LOAI = ages1[i].LOAI;
+        ages3.push(element);
+    }     
+}   
+
+function myFunction3() {
+  var x =document.getElementById("idd1")
+  var xx=`<img src="assets/bvhd.png" style="padding: 5px 0 0 20px; width: 140px;height: 56px;"> `;
+  for(var i = 0; i < ages3.length/4; i++)
+  {
+      var row1 = `<div style="display: flex;" id="id${i}"></div>`
+      xx+=row1   
+      x.innerHTML=xx;    
+  }
+
+  for(var k = 0; k < ages3.length/4; k++)
+  {
+      var tb2 = document.getElementById('id'+k)
+      var tb3 ="";
+      for(var j =k*4; j < 4*k+4; j++)
+      {
+          var row2 = `<div class="wrap">
+                        <img src="${ages3[j].IMG}" alt="Snow" style="width:100%;margin-bottom: 5px;"/>  
+                        <label class="lbl">${ages3[j].GIA}K</label></label>
+                        <span style="margin: 15px; font-weight: 600;">${ages3[j].TENMON}</span>  
+                    </div>`
+              tb3+=row2
+              tb2.innerHTML=tb3    
+      }
+      
+  } 
+}
+
+
+
+var ages4 = [];
+          
+for(var i = 0; i < ages1.length; i++)
+{
+    if(ages1[i].LOAI=="3")
+    {
+        var element = {};
+        element.IMG = ages1[i].IMG;
+        element.TENMON = ages1[i].TENMON;
+        element.GIA = ages1[i].GIA;
+        element.LOAI = ages1[i].LOAI;
+        ages4.push(element);
+    }     
+}   
+
+function myFunction4() {
+  var x =document.getElementById("idd1")
+  var xx=`<img src="assets/bvhd.png" style="padding: 5px 0 0 20px; width: 140px;height: 56px;"> `;
+  for(var i = 0; i < ages4.length/4; i++)
+  {
+      var row1 = `<div style="display: flex;" id="id${i}"></div>`
+      xx+=row1   
+      x.innerHTML=xx;    
+  }
+
+  for(var k = 0; k < ages4.length/4; k++)
+  {
+      var tb2 = document.getElementById('id'+k)
+      var tb3 ="";
+      for(var j =k*4; j < 4*k+4; j++)
+      {
+          var row2 = `<div class="wrap">
+                        <img src="${ages4[j].IMG}" alt="Snow" style="width:100%;margin-bottom: 5px;"/>  
+                        <label class="lbl">${ages4[j].GIA}K</label></label>
+                        <span style="margin: 15px; font-weight: 600;">${ages4[j].TENMON}</span>  
+                    </div>`
+              tb3+=row2
+              tb2.innerHTML=tb3    
+      }
+      
+  } 
 }

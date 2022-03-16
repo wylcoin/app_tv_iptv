2ce5850c62df847be74e9c4913ac6966e5af00da
iron2014hieu <hieuttpk00808@gmail.com>
2022-03-16 18:48:51 +0700

    mm

---
+++ AppTV/js/watchtivi_1.1.js
@@ -0,0 +1,168 @@
+// var handleKeyCode = require('../main.js');
+var $ = document.querySelector.bind(document);
+var items
+var chanelList = [
+  { IMG: "assets/VTV1.png", TEN: "VTV1", id: 1 },
+  { IMG: "assets/VTV3.png", TEN: "VTV3", id: 2 },
+  { IMG: "assets/VTV6.png", TEN: "VTV6", id: 3 },
+  { IMG: "assets/Disney.png", TEN: "Disney", id: 4 },
+  { IMG: "assets/Disney.png", TEN: "Disney", id: 5 },
+  { IMG: "assets/VTV1.png", TEN: "VTV1", id: 6 },
+  { IMG: "assets/VTV3.png", TEN: "VTV3", id: 7 },
+  { IMG: "assets/VTV6.png", TEN: "VTV6", id: 8 },
+  { IMG: "assets/Disney.png", TEN: "Disney", id: 9 },
+  { IMG: "assets/Disney.png", TEN: "Disney", id: 10 },
+  { IMG: "assets/VTV1.png", TEN: "VTV1", id: 11 },
+  { IMG: "assets/VTV3.png", TEN: "VTV3", id: 12 },
+  { IMG: "assets/VTV6.png", TEN: "VTV6", id: 13 },
+  { IMG: "assets/Disney.png", TEN: "Disney", id: 14 },
+  { IMG: "assets/Disney.png", TEN: "Disney", id: 15 },
+  { IMG: "assets/VTV1.png", TEN: "VTV1", id: 16 },
+  { IMG: "assets/VTV3.png", TEN: "VTV3", id: 17 },
+  { IMG: "assets/VTV6.png", TEN: "VTV6", id: 18 },
+  { IMG: "assets/Disney.png", TEN: "Disney", id: 19 },
+  { IMG: "assets/Disney.png", TEN: "Disney", id: 20 },
+  { IMG: "assets/VTV1.png", TEN: "VTV1", id: 21 },
+  { IMG: "assets/VTV3.png", TEN: "VTV3", id: 22 },
+  { IMG: "assets/VTV6.png", TEN: "VTV6", id: 23 },
+  { IMG: "assets/Disney.png", TEN: "Disney", id: 24 },
+  { IMG: "assets/Disney.png", TEN: "Disney", id: 25 },
+];
+
+var list = document.querySelector("#list-tv");
+var html = "";
+
+for (var i = 0; i < chanelList.length / 10; i++) {
+  var rows = '<li class="item-group"> </li>';
+  list.innerHTML += rows;
+
+  var rowsElement = document.querySelectorAll(".item-group");
+  var x = 0;
+  for (var j = i * 10; j < 10 * i + 10; j++) {
+    if (j < chanelList.length) {
+      const item = chanelList[j];
+      // var row = `<a class="item" href="#" data-position="${x}-${i}"><img src="${item.IMG}"  alt="${item.TEN}"></a>`;
+      var row =
+        '<a href="#" class="chanel-item item" data-position="' +
+        x +
+        "-" +
+        i +
+        '">' +
+        '<div class="img-wrap">' +
+        '<img src="' +
+        item.IMG +
+        '" alt="d" />' +
+        "</div>" +
+        '<p class="chanel-name">' +
+        item.TEN +
+        "</p>" +
+        "</a>";
+
+      x++;
+      rowsElement[i].innerHTML += row;
+    }
+  }
+  html += rows;
+}
+
+var defaultPosition = "0-0"; //x-y
+var currentX = Number(defaultPosition.split("-")[0]);
+var currentY = Number(defaultPosition.split("-")[1]);
+
+items = document.querySelectorAll(".item");
+console.log(items);
+
+function loadItemActive() {
+  for (const element of items) {
+    const pos = element.dataset.position;
+    if (pos === defaultPosition) {
+      //   console.log(element);
+      element.classList.add("active");
+    } else element.classList.remove("active");
+  }
+}
+loadItemActive();
+function getNewCurrentPosition() {
+  return currentX + "-" + currentY;
+}
+function isValidNode(location) {
+  var positions = [];
+  for (const node of items) {
+    const position = node.dataset.position;
+    positions.push(position);
+  }
+  var result = positions.indexOf(location)
+
+  if(result === -1) {
+    return false
+  } 
+  return true
+}
+function handlePosition(num, axis) {
+  if (axis === "x") {
+    if (isValidNode(currentX + num + "-" + currentY)) {
+      currentX = currentX + num;
+      handleScrollTvIntoView();
+    }
+  } else {
+    if (isValidNode(currentX + "-" + (currentY + num))) {
+      currentY = currentY + num;
+      handleScrollTvIntoView();
+    }
+  }
+  defaultPosition = getNewCurrentPosition();
+  loadItemActive();
+}
+window.onkeydown = function (e) {
+  console.log(e.keyCode);
+  switch (e.keyCode) {
+    case 37:
+      //   "x--"
+      handlePosition(-1, "x");
+      break;
+    case 38:
+      //   "y--"
+      handlePosition(-1, "y");
+      break;
+    case 39:
+      //   "x++"
+      handlePosition(1, "x");
+      break;
+    case 40:
+      //   "y++"
+      handlePosition(1, "y");
+      break;
+    case 13:
+      for (const node of items) {
+        const locationItem = node.dataset.position;
+
+        if (locationItem === defaultPosition) {
+          // console.log(locationItem,node);
+          node.onclick = function () {
+            console.log("im here ", node);
+          };
+          node.click();
+        }
+      }
+
+      break;
+    case 10009:
+      goBack();
+      break;
+    default:
+      break;
+  }
+};
+
+function handleScrollTvIntoView() {
+  setTimeout(function () {
+    document.querySelector(".item.active").scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline: "center",
+    });
+  }, 350);
+}
+function goBack() {
+  window.history.back();
+}
--- README.md
+++ README.md
@@ -1,1 +1,112 @@
 # app_tv_iptv
+
+## Base api
+
+```js
+var baseAPi = "http://115.78.230.192:59064/api";
+```
+
+## Watch film
+
+```js
+var path = "/film";
+var method = "GET";
+var paramObj = {};
+var dataType: "json";
+```
+
+## Doctor list
+
+```js
+var path = "/doctor";
+var method = "GET";
+var paramObj = {};
+var dataType: "json";
+```
+
+## Order
+
+### Get order
+
+```js
+var path = "/orderdetail";
+var method = "GET";
+var paramObj = {};
+var dataType: "json";
+```
+
+### Get order by id category
+
+- Use filter method with condition:
+
+```js
+item.id_category == "id_category";
+```
+
+### Insert order into cart
+
+```js
+var headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Credentials": "true",
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
+var contentType: "application/json; charset=utf-8";
+
+var path = "/orderdetail";
+var method = "POST";
+var param = {
+  id_food: parseInt(text),
+  id_order: parseInt(idord),
+  quantity: parseInt($("#sl").val()),
+};
+var paramObj = param;
+```
+
+### Delete item of cart
+
+```js
+var headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Credentials": "true",
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
+var contentType: "application/json; charset=utf-8";
+
+var path = "/orderdetail";
+var method = "DELETE";
+var param = {
+  id_food: parseInt(idfood_d),
+  id_order: parseInt(idord),
+};
+var paramObj = param;
+```
+
+### Update cart
+
+```js
+var headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Credentials": "true",
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
+var contentType: "application/json; charset=utf-8";
+
+var path = "/orderdetail";
+var method = "PUT";
+var param = {
+  id_food: parseInt(idfood_d),
+  id_order: parseInt(idord),
+  quantity: parseInt($("#slud").val()),
+};
+var paramObj = param;
+```
+#### Note
+- Use only javascript es5, jquery 1.9.1,... not ~~es6 or higher version~~ 
+- Download Tizen studio [here](https://developer.samsung.com/smarttv/develop/tools/tizen-studio.html) 
+```ts
+Author: by iron2014 - 28/02/2022
+```
